6687f0552e6453049c8024265aaf6bcaa0ce6ebc
Brendan Ly <lybrendan@yahoo.com>
2017-03-23 19:53:29 -0700

    worked on login authentication

--- login.js
+++ login.js
@@ -1,13 +1,13 @@
-function validate(){
-var username = document.getElementById("userid-box").value;
-var password = document.getElementById("pw-box").value;
-if ( username == "username1" && password == "password1"){
-	//onclick = "file:///C:/Users/sungd/Desktop/umhs_cms-master/students_homepage.html";
-	return;
-	}
-  else{
-    alert("Invalid username or password");
-	//onclick = "file:///C:/Users/sungd/Desktop/umhs_cms-master/html/login.html";
-    }
-  return false;
+function validate() {
+  var username = document.getElementById("userid-box").value;
+  var password = document.getElementById("pw-box").value;
+  if (username == "username1" && password == "password1") {
+  	window.location = "students_homepage.html";
+    return false;
   }
+  else {
+    alert("Invalid username or password");
+    //onclick = "file:///C:/Users/sungd/Desktop/umhs_cms-master/html/login.html";
+  }
+return false;
+}
